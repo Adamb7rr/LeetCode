9eca38348254505862cafffc1b07b22762f2bb6c
Adam Bahr <bahrcr7@gmail.com>
2026-05-01 19:10:30 +0300

    [1661] [Time Beats: 53.38%] - LeetPush

--- SQL/Average-Time-of-Process-per-Machine.sql
+++ SQL/Average-Time-of-Process-per-Machine.sql
@@ -1,13 +1,8 @@
-1# Write your MySQL query statement below
-2select 
-3    machine_id,
-4    ROUND(
-5        AVG(
-6            CASE 
-7                WHEN activity_type = 'start' THEN -timestamp 
-8                ELSE timestamp
-9            END
-10        ) * 2, # there are two rows per process_id
-11    3) as processing_time
-12from activity
-13group by machine_id
+1-- Write your PostgreSQL query statement below
+2SELECT machine_id,
+3    ROUND((
+4        AVG(timestamp) FILTER (WHERE activity_type = 'end') -
+5        AVG(timestamp) FILTER (WHERE activity_type = 'start')
+6    )::NUMERIC, 3) AS processing_time
+7FROM Activity
+8GROUP BY machine_id
